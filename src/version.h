544a84e54b3f4e604b56c0160134bf7636e0056a
Velvet78 <sjamraadj@gmail.com>
2016-08-10 15:44:28 +0200

    Update version.h
    updated protocol version and noblks version

--- src/version.h
+++ src/version.h
@@ -30,11 +30,11 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60014;
+static const int PROTOCOL_VERSION = 60015;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
-static const int MIN_PROTO_VERSION_FORK = 60014;
+static const int MIN_PROTO_VERSION_FORK = 60015;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -43,7 +43,7 @@ static const int CADDR_TIME_VERSION = 31402;
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60002;
 static const int NOBLKS_VERSION_END = 60006;
-static const int NOBLKS_VERSION_END_FORK = 60013;
+static const int NOBLKS_VERSION_END_FORK = 60014;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
