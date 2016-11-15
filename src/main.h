5d2cf8c01204ca184ade1d5cafb868e348aa42a1
Velvet78 <sjamraadj@gmail.com>
2016-11-15 16:52:58 +0100

    Update main.h

--- src/main.h
+++ src/main.h
@@ -39,6 +39,7 @@ static const int64_t MAX_MONEY = 235311570 * COIN;
 static const int64_t COIN_YEAR_REWARD = 1337 * CENT; // 1337%, After fork1 668.5%, after fork2 334.25%
 static const unsigned int FORK_TIME = 1459695600; //  Sun, 03 Apr 2016 15:00:00 GMT
 static const unsigned int FORK_TIME2 = 1472125032; //  Thu, 25 Aug 2016 11:37:12 GMT
+static const unsigned int FORK_TIME3 = 1480081025; //  Fri, 25 Nov 2016 13:37:05 GMT
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
@@ -132,6 +133,7 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees, unsigned int nTim
 int64_t GetProofOfStakeRewardV1(int64_t nCoinAge, int64_t nFees);
 int64_t GetProofOfStakeRewardV2(int64_t nCoinAge, int64_t nFees);
 int64_t GetProofOfStakeRewardV3(int64_t nCoinAge, int64_t nFees);
+int64_t GetProofOfStakeRewardV4(int64_t nCoinAge, int64_t nFees);
 unsigned int ComputeMinWork(unsigned int nBase, int64_t nTime);
 unsigned int ComputeMinStake(unsigned int nBase, int64_t nTime, unsigned int nBlockTime);
 int GetNumBlocksOfPeers();
