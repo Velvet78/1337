3b12c66e7367d47a6c28a7f5725b5b2d6136841a
Velvet78 <sjamraadj@gmail.com>
2017-03-11 17:22:35 +0100

    sig fix

--- src/key.cpp
+++ src/key.cpp
@@ -441,6 +441,18 @@ bool CKey::Verify(uint256 hash, const std::vector<unsigned char>& vchSigParam)
     return ret;
 }
 
+
+bool CKey::VerifyCompact(uint256 hash, const std::vector<unsigned char>& vchSig)
+{
+    CKey key;
+    if (!key.SetCompactSignature(hash, vchSig))
+        return false;
+    if (GetPubKey() != key.GetPubKey())
+        return false;
+
+    return true;
+}
+
 bool CKey::IsValid()
 {
     if (!fSet)
